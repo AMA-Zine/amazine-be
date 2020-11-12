b353e004023758b872b54bcc96d6306bdc885b37
SarahRector <rector.sarah.r@gmail.com>
2020-11-12 13:51:18 -0800

    added SQL tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,19 @@
-DROP TABLE IF EXISTS tablename;
+DROP TABLE IF EXISTS threads;
 
-CREATE TABLE tablename (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
-)
+CREATE TABLE threads (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title TEXT NOT NULL,
+  author TEXT NOT NULL,
+  flair TEXT,
+  upvotes INT,
+  downvotes INT,
+  image TEXT
+);
+
+DROP TABLE IF EXISTS responses;
+
+CREATE TABLE responses (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  q_and_a JSONB,
+  thread_id BIGINT NOT NULL REFERENCES threads(id)
+);
